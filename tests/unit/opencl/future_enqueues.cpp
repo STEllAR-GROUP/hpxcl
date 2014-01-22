349e6af532259604102653aef1b91c847ebd6d40
Martin Stumpf <martin.h.stumpf@gmail.com>
2014-01-22 20:41:23 +0100

    modified future_enqueues because one case was missing

--- tests/unit/opencl/future_enqueues.cpp
+++ tests/unit/opencl/future_enqueues.cpp
@@ -20,17 +20,15 @@
  *          |-> X = A+B -|
  * write_B -|            |
  *                       |-> Z = X * Y -> read_Z
- * write_C -|            |
- *          |-> Y = C-D -|
- * write_D -|
+ * write_C ---> Y = 2*C -|
+ *
  */
 
 static const int inputA[] = {  1,  5,  2,  4,  3};
 static const int inputB[] = {  3,  7,  4,  1,  4};
 static const int inputC[] = {  4,  9,  4,  7,  8};
-static const int inputD[] = {  6,  3,  7,  2,  1};
 static const int refX[]   = {  4, 12,  6,  5,  7};
-static const int refY[]   = { -2,  6, -3,  5,  7};
+static const int refY[]   = {  8, 18,  8, 14, 16};
 static const int refZ[]   = { -8, 72,-18, 25, 49}; 
 static const size_t DATASIZE = 5 * sizeof(int);
 
@@ -43,11 +41,10 @@ static const char gpu_prog[] =
 "       out[tid] = in1[tid] + in2[tid];                             \n"
 "   }                                                               \n"
 "                                                                   \n"
-"   __kernel void sub(__global int* out,__global int* in1,          \n"
-"                                       __global int* in2)          \n"
+"   __kernel void dbl(__global int* out,__global int* in)           \n"
 "   {                                                               \n"
 "       size_t tid = get_global_id(0);                              \n"
-"       out[tid] = in1[tid] - in2[tid];                             \n"
+"       out[tid] = 2 * in[tid];                                     \n"
 "   }                                                               \n"
 "                                                                   \n"
 "   __kernel void mul(__global int* out,__global int* in1,          \n"
@@ -85,7 +82,7 @@ static void cl_test(hpx::opencl::device cldevice)
     hpx::opencl::program prog = cldevice.create_program_with_source(gpu_prog);
     prog.build();
     hpx::opencl::kernel add_kernel = prog.create_kernel("add");
-    hpx::opencl::kernel sub_kernel = prog.create_kernel("sub");
+    hpx::opencl::kernel dbl_kernel = prog.create_kernel("dbl");
     hpx::opencl::kernel mul_kernel = prog.create_kernel("mul");
 
     // This trigger will start the entire process.
@@ -102,8 +99,6 @@ static void cl_test(hpx::opencl::device cldevice)
                                                             DATASIZE);
     hpx::opencl::buffer bufC = cldevice.create_buffer(CL_MEM_READ_WRITE,
                                                             DATASIZE);
-    hpx::opencl::buffer bufD = cldevice.create_buffer(CL_MEM_READ_WRITE,
-                                                            DATASIZE);
     hpx::opencl::buffer bufX = cldevice.create_buffer(CL_MEM_READ_WRITE,
                                                             DATASIZE);
     hpx::opencl::buffer bufY = cldevice.create_buffer(CL_MEM_READ_WRITE,
@@ -114,29 +109,23 @@ static void cl_test(hpx::opencl::device cldevice)
     // Write to buffers
     future_event initAevent = bufA.enqueue_write(0, DATASIZE, inputA,
                                                                     startEvent);
-    future_event initBevent = bufA.enqueue_write(0, DATASIZE, inputB,
+    future_event initBevent = bufB.enqueue_write(0, DATASIZE, inputB,
                                                                     startEvent);
-    future_event initCevent = bufA.enqueue_write(0, DATASIZE, inputC,
-                                                                    startEvent);
-    future_event initDevent = bufA.enqueue_write(0, DATASIZE, inputD,
+    future_event initCevent = bufC.enqueue_write(0, DATASIZE, inputC,
                                                                     startEvent);
     
     // Create dependency lists
     std::vector<future_event> initABevent(2);
     initABevent.push_back(initAevent);
     initABevent.push_back(initBevent);
-    std::vector<future_event> initCDevent(2);
-    initCDevent.push_back(initCevent);
-    initCDevent.push_back(initDevent);
 
     // set kernel args
     add_kernel.set_arg(0, bufX);
     add_kernel.set_arg(1, bufA);
     add_kernel.set_arg(2, bufB);
 
-    sub_kernel.set_arg(0, bufY);
-    sub_kernel.set_arg(1, bufC);
-    sub_kernel.set_arg(2, bufD);
+    dbl_kernel.set_arg(0, bufY);
+    dbl_kernel.set_arg(1, bufC);
     
     mul_kernel.set_arg(0, bufZ);
     mul_kernel.set_arg(1, bufX);
@@ -147,17 +136,17 @@ static void cl_test(hpx::opencl::device cldevice)
     dim[0].offset = 0;
     dim[0].size = 5;
 
-    // run add and sub kernels
+    // run add and dbl kernels
     future_event add_event = add_kernel.enqueue(dim, initABevent);
-    future_event sub_event = sub_kernel.enqueue(dim, initCDevent);
+    future_event dbl_event = dbl_kernel.enqueue(dim, initCevent);
 
     // combine events
-    std::vector<future_event> add_sub_event(2);
-    add_sub_event.push_back(add_event);
-    add_sub_event.push_back(sub_event);
+    std::vector<future_event> add_dbl_event(2);
+    add_dbl_event.push_back(add_event);
+    add_dbl_event.push_back(dbl_event);
 
     // run mul kernel
-    future_event mul_event = mul_kernel.enqueue(dim, add_sub_event);
+    future_event mul_event = mul_kernel.enqueue(dim, add_dbl_event);
 
     // run read from result buffer
     future_event readZevent = bufZ.enqueue_read(0, DATASIZE, mul_event);
