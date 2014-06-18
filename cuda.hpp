7e6825edaf3387fdb850cb5d0610d7adc8ed5cdb
Damond Howard <dhoward@deneb.hermione>
2014-06-18 15:45:32 -0500

    add cuda.hpp

---
+++ cuda.hpp
@@ -0,0 +1,15 @@
+// Copyright (c)    2013 Martin Stumpf
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#pragma once
+#ifndef HPX_CUDA_HPP_
+#define HPX_CUDA_HPP_
+
+    #include "cuda/device.hpp"
+    //#include "cuda/event.hpp"
+    //#include "cuda/buffer.hpp"
+    #include "cuda/kernel.hpp"
+
+#endif
