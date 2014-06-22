e827dc714a311097a6c13fdbfd6161da885a1080
Finomnis <martin.h.stumpf@gmail.com>
2014-06-21 21:11:51 -0500

    mandelbrot: rewrote pngwriter

--- examples/opencl/mandelbrot/pngwriter.hpp
+++ examples/opencl/mandelbrot/pngwriter.hpp
@@ -6,11 +6,23 @@
 #ifndef MANDELBROT_PNG_WRITER_H_
 #define MANDELBROT_PNG_WRITER_H_
 
+
 #include <cstdlib>
+#include <vector>
 #include <boost/shared_ptr.hpp>
+#include <boost/shared_array.hpp>
 
 // writes data to png file
-void save_png(boost::shared_ptr<std::vector<char>> data, size_t width, size_t height, const char* filename);
+void save_png(boost::shared_ptr< std::vector<char> > data, size_t width, size_t height, const char* filename);
+
+boost::shared_array<char> create_png(boost::shared_ptr< std::vector<char> > data, size_t width, size_t height, size_t * size);
+
+void png_write_to_file(boost::shared_array<char> png, size_t png_size, const char* filename);
+
+
+
+
+
 
 #endif
 
