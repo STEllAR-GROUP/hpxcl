ac75ef74fd6e674f38664c933d378c36001df0a6
Finomnis <martin.h.stumpf@gmail.com>
2014-07-01 12:28:46 -0500

    mandelbrot: added grainsize bench

--- examples/opencl/mandelbrot/main.cpp
+++ examples/opencl/mandelbrot/main.cpp
@@ -18,6 +18,169 @@
 #include <string>
 #include <boost/shared_ptr.hpp>
 
+
+
+void grainsize_bench(std::vector<hpx::opencl::device> devices,
+                   size_t img_x, size_t img_y,
+                   size_t num_iterations, size_t num_kernels, bool verbose)
+{
+    
+            // default benchmark image
+            double posx = -0.743643887037151;
+            double posy = 0.131825904205330;
+            double zoom = 6.2426215349789484160e10;
+
+            std::cerr << "Starting in benchmark mode." << std::endl;
+
+            // create image generator without gpus
+            image_generator img_gen(1, 1, num_kernels, verbose, devices);
+
+            img_gen.wait_for_startup_finished();
+
+            // iterate through all configurations
+            for(size_t grainsize = 512; grainsize <= img_x*img_y; grainsize *= 2)
+            {
+                std::cerr << "Starting test with grainsize " << grainsize << " ..."
+                          << std::endl;
+               
+                // calculate tile size
+                size_t tilesize_x, tilesize_y;
+                if(grainsize < img_x)
+                {
+                    tilesize_y = 1;
+                    tilesize_x = grainsize;
+                }
+                else
+                {
+                    tilesize_x = img_x;
+                    tilesize_y = grainsize/img_x;
+                }
+               
+                std::cerr << "Using tilesizes: " << tilesize_x << "x"
+                          << tilesize_y << std::endl;
+
+                // main benchmark loop
+                for(size_t i = 0; i < num_iterations + 1; i++)
+                {
+                    if(i == 0)
+                    {
+                        std::cerr << "Warmup iteration ..." << std::endl;
+                    }
+                    if(i == 1)
+                    {
+                        std::cerr << "Starting benchmark ..." << std::endl;
+                        // start timer after first iteration (warmup iteration)
+                        if(i == 1) timer_start();
+                    }
+                    img_gen.compute_image(posx,
+                                          posy,
+                                          zoom,
+                                          0.0,
+                                          img_x,
+                                          img_y,
+                                          true,
+                                          tilesize_x,
+                                          tilesize_y).get();
+                }
+
+                // stop timer
+                double time = timer_stop();
+                time = time / (double)num_iterations - 1;
+                
+                std::cerr << "Time: " << time << " ms" << hpx::endl;
+                std::cout << grainsize
+                          << "\t" << time 
+                          << hpx::endl;
+
+            }
+
+            std::cerr << "Done." << hpx::endl;
+            img_gen.shutdown();
+
+   
+    
+    
+}
+
+void speedup_bench(std::vector<hpx::opencl::device> devices,
+                   size_t tilesize_x, size_t tilesize_y, size_t img_x, size_t img_y,
+                   size_t num_iterations, size_t num_kernels, bool verbose)
+{
+    
+            // default benchmark image
+            double posx = -0.743643887037151;
+            double posy = 0.131825904205330;
+            double zoom = 6.2426215349789484160e10;
+
+            std::cerr << "Starting in benchmark mode." << std::endl;
+
+            // create image generator without gpus
+            image_generator img_gen(tilesize_x, tilesize_y, num_kernels, verbose);
+
+            // save the time for single-gpu
+            double single_gpu_time = 1.0f;
+
+            for(size_t num_gpus = 1; num_gpus <= devices.size(); num_gpus++)
+            {
+                std::cerr << "Starting test with " << num_gpus << " gpus ..."
+                          << std::endl;
+               
+                // Add another worker
+                if(verbose){
+                     hpx::cerr << "adding worker ..."
+                               << hpx::endl;
+                }
+                img_gen.add_worker(devices[devices.size() - num_gpus], 4); 
+
+                // Wait for the worker to initialize
+                if(verbose){
+                     hpx::cerr << "waiting for worker to finish startup ..."
+                               << hpx::endl;
+                }
+                img_gen.wait_for_startup_finished();
+
+                // main benchmark loop
+                for(size_t i = 0; i < num_iterations + 1; i++)
+                {
+                    // start timer after first iteration (warmup iteration)
+                    if(i == 1) timer_start();
+                    img_gen.compute_image(posx,
+                                          posy,
+                                          zoom,
+                                          0.0,
+                                          img_x,
+                                          img_y,
+                                          true,
+                                          tilesize_x,
+                                          tilesize_y).get();
+                }
+
+                // stop timer
+                double time = timer_stop();
+                time = time / (double)num_iterations - 1;
+                
+                // save time if we only have one gpu
+                if(num_gpus == 1)
+                    single_gpu_time = time;
+                
+                std::cerr << "Time: " << time << " ms" << hpx::endl;
+                std::cout << num_gpus 
+                          << "\t" << time 
+                          << "\t" << (single_gpu_time / time) 
+                          << "\t" << ((single_gpu_time / time) / (double)num_gpus)
+                          << hpx::endl;
+
+            }
+
+            std::cerr << "Done." << hpx::endl;
+            img_gen.shutdown();
+
+   
+    
+    
+}
+
+
 int hpx_main(boost::program_options::variables_map & vm)
 {
 
@@ -101,75 +264,19 @@ int hpx_main(boost::program_options::variables_map & vm)
             save_png(img_data, img_x, img_y, "test.png");
         
         } else {
-
-            size_t num_iterations = 10;
-
-            std::cerr << "Starting in benchmark mode." << std::endl;
-            size_t chunksize = 8;
-
-            // create image generator without gpus
-            image_generator img_gen(img_x, chunksize, num_kernels, verbose);
-
-            // save the time for single-gpu
-            double single_gpu_time = 1.0f;
-
-            for(size_t num_gpus = 1; num_gpus <= devices.size(); num_gpus++)
-            {
-                std::cerr << "Starting test with " << num_gpus << " gpus ..."
-                          << std::endl;
-               
-                // Add another worker
-                if(verbose){
-                     hpx::cerr << "adding worker ..."
-                               << hpx::endl;
-                }
-                img_gen.add_worker(devices[devices.size() - num_gpus], 4); 
-
-                // Wait for the worker to initialize
-                if(verbose){
-                     hpx::cerr << "waiting for worker to finish startup ..."
-                               << hpx::endl;
-                }
-                img_gen.wait_for_startup_finished();
-
-                // Start timing
-
-                // Add image
-                for(size_t i = 0; i < num_iterations + 1; i++)
-                {
-                    if(i == 1) timer_start();
-                    img_gen.compute_image(posx,
-                                          posy,
-                                          zoom,
-                                          0.0,
-                                          img_x,
-                                          img_y,
-                                          true,
-                                          img_x,
-                                          chunksize).get();
-                }
-
-                // stop timer
-                double time = timer_stop();
-                time = time / (double)num_iterations - 1;
-
-                // save time if we only have one gpu
-                if(num_gpus == 1)
-                    single_gpu_time = time;
-
-                
-                std::cerr << "Time: " << time << " ms" << hpx::endl;
-                std::cout << num_gpus 
-                          << "\t" << time 
-                          << "\t" << (single_gpu_time / time) 
-                          << "\t" << ((single_gpu_time / time) / (double)num_gpus)
-                          << hpx::endl;
-
-            }
-
-            std::cerr << "Done." << hpx::endl;
-            img_gen.shutdown();
-
+            
+            /*speedup_bench(devices,
+                          3840, 8,
+                          3840, 2160,
+                          10,
+                          4,
+                          verbose);
+*/
+            grainsize_bench(devices,
+                            4096, 2048,
+                            10,
+                            4,
+                            verbose);
         }
 
     }
@@ -200,7 +307,7 @@ int main(int argc, char* argv[])
 
     cmdline.add_options()
         ( "bench"
-        , "disables writing to files") ;
+        , "runs benchmark") ;
 
     return hpx::init(cmdline, argc, argv);
 }
