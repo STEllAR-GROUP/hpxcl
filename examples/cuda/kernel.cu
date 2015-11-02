2a41870f21522b1889e3070c181eba9c27b97779
Patrick Diehl <diehl@ins.uni-bonn.de>
2015-11-02 17:45:09 +0100

    Add missing files for the kernel example

---
+++ examples/cuda/kernel.cu
@@ -0,0 +1,9 @@
+
+extern "C" __global__ void sum(unsigned int* array, unsigned int* count,
+		unsigned int* n) {
+	for (int i = blockDim.x * blockIdx.x + threadIdx.x; i < n[0];
+			i += gridDim.x * blockDim.x) {
+		atomicAdd(&(count[0]), array[i]);
+	}
+}
+;
