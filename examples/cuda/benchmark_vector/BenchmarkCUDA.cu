735ae873db08cc527a2f63208799079afc6bdf8a
Patrick Diehl <diehl@ins.uni-bonn.de>
2015-11-10 17:01:43 +0100

    Add missing copyright to the new files

--- examples/cuda/benchmark_vector/BenchmarkCUDA.cu
+++ examples/cuda/benchmark_vector/BenchmarkCUDA.cu
@@ -1,3 +1,8 @@
+// Copyright (c)       2015 Patrick Diehl
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 #include "utils.hpp"
 #include <cuda.h>
 #include <iostream>
@@ -5,8 +10,8 @@
 
 #include "examples/opencl/benchmark_vector/timer.hpp"
 
-#define SINGLE
-#define EPS 10e-5
+//#define SINGLE
+#define EPS 10e-6
 //###########################################################################
 //Switching between single precision and double precision
 //###########################################################################
--- examples/cuda/kernel.cu
+++ examples/cuda/kernel.cu
@@ -1,3 +1,7 @@
+// Copyright (c)       2015 Patrick Diehl
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 extern "C" __global__ void sum(unsigned int* array, unsigned int* count,
 		unsigned int* n) {
