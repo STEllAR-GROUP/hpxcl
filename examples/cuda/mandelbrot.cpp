3bc46601bb52f08c73b12445f2a5f19cfd099095
Madhavan Seshadri <MADHAVAN001@e.ntu.edu.sg>
2017-05-20 04:45:26 +0800

    Started the work on Mandelbrot

---
+++ examples/cuda/mandelbrot.cpp
@@ -0,0 +1,27 @@
+// Copyright (c)       2017 Madhavan Seshadri
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <hpx/hpx_main.hpp>
+#include <hpx/include/iostreams.hpp>
+#include <hpx/lcos/future.hpp>
+#include <hpx/hpx_main.hpp>
+
+#include <hpxcl/cuda.hpp>
+
+using namespace hpx::cuda;
+
+//###########################################################################
+//Kernels
+//###########################################################################
+
+
+
+//###########################################################################
+//Main
+//###########################################################################
+
+int main(){
+	return EXIT_SUCCESS;
+}
