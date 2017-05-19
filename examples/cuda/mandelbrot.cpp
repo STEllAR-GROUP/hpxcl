64e69338091d09006dcf99e3a1f6d3ef7fc8fb69
Madhavan Seshadri <MADHAVAN001@e.ntu.edu.sg>
2017-05-20 05:09:23 +0800

    Started the work on Mandelbrot

--- examples/cuda/mandelbrot.cpp
+++ examples/cuda/mandelbrot.cpp
@@ -16,12 +16,25 @@ using namespace hpx::cuda;
 //Kernels
 //###########################################################################
 
-
+static const char kernel_src[] = "";
 
 //###########################################################################
 //Main
 //###########################################################################
 
-int main(){
+int main(int argc, char* argv[]){
+
+	//Reading a list of available devices in hpx locality
+	//Returns a future
+	std::std::vector<device> devices = get_all_devices(2, 0).get();
+
+	if(devices.size() < 1) {
+		hpx::cerr << "No CUDA devices found! Terminating...." << hpx::endl;
+		return hpx::finalize();
+	}
+	
+
+
+
 	return EXIT_SUCCESS;
 }
