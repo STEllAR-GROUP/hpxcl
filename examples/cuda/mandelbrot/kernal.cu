d8c09c0944ba3e6f4077b79de43675d231f67655
Madhavan Seshadri <MADHAVAN001@e.ntu.edu.sg>
2017-05-24 02:37:31 +0800

    added kernel

---
+++ examples/cuda/mandelbrot/kernal.cu
@@ -0,0 +1,33 @@
+__global void kernel(char *out, int width, int height, int numIterations){
+	unsigned int xDim = blockId.x * blockDim.x + threadId.x;
+	unsigned int yDim = blockId.y * blockDim.y + threadId.y;
+
+	//index of the output array, multiplied by 3 for R,G,B values
+	int arrayIndex = 3 * width * y_dim + x_dim*3;
+
+	float xPoint = ((float) xDim/width)*3.25 - 2;
+	float yPoint = ((float) yDim/width)*2.5 - 1.25; 
+
+	//for calculation of complex number
+	float x = 0;
+	float y = 0;
+
+	int iterationCount = 0;
+	//terminating condition x^2+y^2 < 4 or iterations >numIterations
+	while(y*y+x*x<=4 && iterationCount<numIterations){
+		xTemp = x*x-y*y + xPoint;
+		y = 2*x*y + yPoint;
+		x = xTemp;
+		iterationCount++;
+	}
+
+	if(iterationCount == numIterations){
+		out[arrayIndex] = 0;
+		out[arrayIndex+1]=0;
+		out[arrayIndex+2]=0;
+	}else{
+		out[arrayIndex] = iterationCount;
+		out[arrayIndex+1]=iterationCount;
+		out[arrayIndex+2]=iterationCount;
+	}
+}
