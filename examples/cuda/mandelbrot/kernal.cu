0aebbb73c0b9c0b0c87456eb9886dfefeda8540f
Madhavan Seshadri <MADHAVAN001@e.ntu.edu.sg>
2017-06-06 10:27:04 +0530

    Added license

--- examples/cuda/mandelbrot/kernal.cu
+++ examples/cuda/mandelbrot/kernal.cu
@@ -1,3 +1,7 @@
+// Copyright (c)       2017 Madhavan Seshadri
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 extern "C" __global__ void kernel(char *out, int *width, int *height, int *yStart){
 	unsigned int xDim = blockIdx.x * blockDim.x + threadIdx.x;
 	unsigned int yDim = blockIdx.y * blockDim.y + threadIdx.y;
