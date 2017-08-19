a48de4afe31251af471f32032670b09e683f3e05
Madhavan Seshadri <MADHAVAN001@e.ntu.edu.sg>
2017-08-20 00:33:11 +0800

    Added HPX Opencl smvp

--- benchmark/opencl/smvp/smvpHPX.cpp
+++ benchmark/opencl/smvp/smvpHPX.cpp
@@ -94,18 +94,18 @@ int main(int argc, char* argv[])
 	count[0] = 0;
 
 	//Input can be anything sparse
-	for (i = 0; i < (m*n); i++) {
-		if((i%n) == 0){
+	for (i = 0; i < (m[0]*n[0]); i++) {
+		if((i%n[0]) == 0){
 			A[i] = (double)(i+1);
-			count++;
+			count[0]++;
 		}
 	}
 
-	for (i = 0; i < (1*n); i++) {
+	for (i = 0; i < (1*n[0]); i++) {
 		B[i] = (double)(-i-1);
 	}
 
-	for (i = 0; i < (m*1); i++) {
+	for (i = 0; i < (m[0]*1); i++) {
 		C[i] = 0.0;
 	}
 
@@ -115,19 +115,19 @@ int main(int argc, char* argv[])
 	int pointer_counter = -1;
 
 	//Compressing Matrix A
-	for (i = 0; i < (m*n); i++) {
+	for (i = 0; i < (m[0]*n[0]); i++) {
 		if(A[i] != 0)
 		{
 			A_data[data_counter++] = A[i];
-			if(((int)i/n) != pointer_counter)
+			if(((int)i/n[0]) != pointer_counter)
 				A_pointers[++pointer_counter] = index_counter;
-			A_indices[index_counter++] = (i%n);
+			A_indices[index_counter++] = (i%n[0]);
 		}
 	}
 
 	//creating buffers
-	buffer ADataBuffer = cldevice.create_buffer(CL_MEM_READ_ONLY, (*count)*sizeof( int ));
-	buffer AIndexBuffer = cldevice.create_buffer(CL_MEM_READ_ONLY, (*count)*sizeof( int ));
+	buffer ADataBuffer = cldevice.create_buffer(CL_MEM_READ_ONLY, (count[0])*sizeof( int ));
+	buffer AIndexBuffer = cldevice.create_buffer(CL_MEM_READ_ONLY, (count[0])*sizeof( int ));
 	buffer APointerBuffer = cldevice.create_buffer(CL_MEM_READ_ONLY, m[0]*sizeof( double ));	
 
 	buffer BBuffer = cldevice.create_buffer(CL_MEM_READ_ONLY, n[0]*sizeof( double ));
@@ -202,15 +202,15 @@ int main(int argc, char* argv[])
     kernel smvp_kernel = prog.create_kernel("smvp");
 
     //Set buffers as arguments
-    set_arg_futures.push_back(dgemm_kernel.set_arg_async(0, ADataBuffer));
-    set_arg_futures.push_back(dgemm_kernel.set_arg_async(1, AIndexBuffer));
-    set_arg_futures.push_back(dgemm_kernel.set_arg_async(2, APointerBuffer));
-    set_arg_futures.push_back(dgemm_kernel.set_arg_async(3, BBuffer));
-    set_arg_futures.push_back(dgemm_kernel.set_arg_async(4, CBuffer));
-    set_arg_futures.push_back(dgemm_kernel.set_arg_async(5, mBuffer));
-    set_arg_futures.push_back(dgemm_kernel.set_arg_async(6, nBuffer));
-    set_arg_futures.push_back(dgemm_kernel.set_arg_async(7, countBuffer));
-    set_arg_futures.push_back(dgemm_kernel.set_arg_async(8, alphaBuffer));
+    set_arg_futures.push_back(smvp_kernel.set_arg_async(0, ADataBuffer));
+    set_arg_futures.push_back(smvp_kernel.set_arg_async(1, AIndexBuffer));
+    set_arg_futures.push_back(smvp_kernel.set_arg_async(2, APointerBuffer));
+    set_arg_futures.push_back(smvp_kernel.set_arg_async(3, BBuffer));
+    set_arg_futures.push_back(smvp_kernel.set_arg_async(4, CBuffer));
+    set_arg_futures.push_back(smvp_kernel.set_arg_async(5, mBuffer));
+    set_arg_futures.push_back(smvp_kernel.set_arg_async(6, nBuffer));
+    set_arg_futures.push_back(smvp_kernel.set_arg_async(7, countBuffer));
+    set_arg_futures.push_back(smvp_kernel.set_arg_async(8, alphaBuffer));
     
     // wait for function calls to trigger
     hpx::wait_all( set_arg_futures );
