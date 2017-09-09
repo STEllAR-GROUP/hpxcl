2df06a85c02a8fa44729de29888d270b17dfb384
Patrick Diehl <diehlpk@users.noreply.github.com>
2017-09-09 09:29:39 -0400

    Add proper extern C style

--- benchmark/cuda/smvp/smvp.cu
+++ benchmark/cuda/smvp/smvp.cu
@@ -3,7 +3,7 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-extern "C" __global__ void smvp(double *A_data, int *A_indices, int *A_pointers, double *B, double *C, int *m, int *n, int *count, double *alpha){
+extern "C" { __global__ void smvp(double *A_data, int *A_indices, int *A_pointers, double *B, double *C, int *m, int *n, int *count, double *alpha){
 	int ROW = blockIdx.x*blockDim.x+threadIdx.x;
 
 	if(ROW<*m){
@@ -18,5 +18,5 @@ extern "C" __global__ void smvp(double *A_data, int *A_indices, int *A_pointers,
 		}
 		C[ROW] = sum;
 	}	
-
+}
 }
