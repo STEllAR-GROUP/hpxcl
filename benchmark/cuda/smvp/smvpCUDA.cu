f837fb7c60982e66c73c56c3cc522f91ddad494f
Madhavan Seshadri <MADHAVAN001@e.ntu.edu.sg>
2017-09-17 23:54:58 +0800

    Added SMVP Validation

--- benchmark/cuda/smvp/smvpCUDA.cu
+++ benchmark/cuda/smvp/smvpCUDA.cu
@@ -160,7 +160,7 @@ int main(int argc, char*argv[]) {
     std:: cout << time << " ";
 
 	// Validating the result
-	std::cout << validateSmvp(A_data, A_indices, A_pointers, B, C, m, n, count, alpha) << std::endl;
+	std::cout << validateSmvp(A_data, A_indices, A_pointers, B, C, &m, &n, &count, &alpha) << std::endl;
 
 	return EXIT_SUCCESS;
 }
--- benchmark/cuda/smvp/validation.h
+++ benchmark/cuda/smvp/validation.h
@@ -11,28 +11,26 @@
 int validateSmvp(double *A_data, int *A_indices, int *A_pointers, double *B, double *C, int *m, int *n, int *count, double *alpha) {
 
 	double * CTest;
-	cudaMallocHost((void**) &CTest, m * 1 * sizeof(double));
+	cudaMallocHost((void**) &CTest, (*m) * 1 * sizeof(double));
 	
 	std::memset(CTest, 0, sizeof CTest);
 
-
-	for( size_t i = 0; i < m; i++) {
+	for( size_t i = 0; i < (*m); i++) {
 		int start = A_pointers[i];
-		int end = (start==m-1)?(count):A_pointers[i+1];
+		int end = (start==(*m)-1)?(*count):A_pointers[i+1];
 
 		double sum = 0;
 		for(int j = start;j<end;j++)
 		{
 			int index = A_indices[j];
-			sum += (alpha) * A_data[j] * B[index];
+			sum += (*alpha) * A_data[j] * B[index];
 		}
 		CTest[i] = sum;
 	}
 
-
 	int success = 1;
 
-	for( size_t i = 0; i < m; i++)
+	for( size_t i = 0; i < (*m); i++)
 
 		if (std::abs(CTest[i] - C[i]) > EPS){
 			success = 0;
