273bf7de1fd9e0e9243c2cf52b8537cea55be00e
Patrick Diehl <patrickdiehl@lsu.edu>
2021-04-16 21:07:09 -0500

    Revert "Update headers (#85)"
    This reverts commit e16016dbb3f93705691edfd6a95413773505ccd4.

--- examples/cuda/cuda_list_extended_devices.cpp
+++ examples/cuda/cuda_list_extended_devices.cpp
@@ -3,8 +3,8 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#include <hpx/iostream.hpp>
-#include <hpx/future.hpp>
+#include <hpx/include/iostreams.hpp>
+#include <hpx/lcos/future.hpp>
 #include <hpx/hpx_main.hpp>
 
 #include <hpxcl/cuda.hpp>
