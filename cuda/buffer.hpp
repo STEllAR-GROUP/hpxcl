408ff3fbe696209444a24dc12ecf314fe3cc63fd
Patrick Diehl <patrickdiehl@lsu.edu>
2021-07-06 15:44:03 -0500

    Add comments

--- cuda/buffer.hpp
+++ cuda/buffer.hpp
@@ -94,9 +94,11 @@ public:
 
 	}
 
-    
+    /** 
+     * \brief Method to access the device pointer wrapped as a smart pointer
+     * \return The pointer to the device memory as a smart pointer
+     */
     hpx::lcos::future<std::shared_ptr<size_t>> get_smart_pointer(){
-    
     
 		HPX_ASSERT(this->get_id());
         
@@ -105,11 +107,20 @@ public:
     }
 
     
+    /** 
+     * \brief Method to access the device pointer wrapped as a smart pointer
+     * \return The pointer to the device memory as a smart pointer
+     */
     std::shared_ptr<size_t> get_smart_pointer_sync(){
     
         return get_smart_pointer().get();
     }
-    
+
+
+    /**
+     * \brief Method to access the device pointer
+     * \return The raw pointer to the device memory allocated in this buffer
+     */
     void* get_raw_pointer_sync(){
     
     return reinterpret_cast<void*>(get_smart_pointer_sync().get());
